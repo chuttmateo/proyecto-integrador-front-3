ead64dcb155bc7c4199e3dc1f01de0e670c6baed
mateobr0 <chuttmateo0@gmail.com>
2023-06-26 23:59:03 -0300

    prueba deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/proyecto-integrador-front-3">
       <App />
     </BrowserRouter>
   </React.StrictMode>
